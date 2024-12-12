f9c8f76f4be72e93dcaa259bb790119360bf2459
lkakitani <leonardo.kakitani@gmail.com>
2024-12-12 12:04:16 -0300

    Add item details to cart recovery event (#1)
    * Add to cart event - context variables
    
    * Fix syntax
    
    * Update brand logo
    
    * Add more events and fixed older ones

--- react/index.tsx
+++ react/index.tsx
@@ -7,18 +7,34 @@ window.IlabsPush = window.IlabsPush || []
 export function handleEvents(e: PixelMessage) {
   switch (e.data.eventName) {
     case 'vtex:addToCart': {
-      window.IlabsPush.push(["ecommerce.itemAddedToCart"])
-      break
-    }
-    case 'vtex:pageView': {
-      if (e.data.pageUrl.match("checkout/orderPlaced")) {
-        window.IlabsPush.push(["ecommerce.checkoutCompleted"]);
-      }
+      let product = e.data.items[0] ?? {};
+      window.IlabsPush.push(["ecommerce.itemAddedToCart", {
+        context: {
+          name: product.name ?? '',
+          imageUrl: product.imageUrl ?? '',
+          price: product.price ? (product.price / 100).toLocaleString('pt-BR', { minimumFractionDigits: 2 }) : '',
+          url: window.location.href,
+        }
+      }])
       break
     }
     case 'vtex:orderPlacedTracked':
     case 'vtex:orderPlaced': {
-      window.IlabsPush.push(["ecommerce.checkoutCompleted"]);
+      window.IlabsPush.push(["ecommerce.checkoutCompleted", {
+        context: {
+          amount: e.data.transactionTotal,
+          currency: e.data.transactionCurrency,
+          totalProducts: e.data.transactionProducts.reduce((acc, product) => acc + product.quantity, 0),
+          url: window.location.href,
+        }
+      }]);
+      break
+    }
+    case 'vtex:cartChanged': {
+      let isCartEmpty = e.data.items.length === 0;
+      if (isCartEmpty) {
+        window.IlabsPush.push(["ecommerce.cartEmptied"]);
+      }
       break
     }
 
